bdb058fd84ddb24e0ddb7a5a5f9c29aa524c9a1c
celpit <celpit17@gmail.com>
2020-03-15 12:28:42 -0500

    fixed textsummary

--- textsummary.py
+++ textsummary.py
@@ -9,65 +9,50 @@ def textsummary(input):
     lettercount = 0
     vowelcount = 0
     sentencecount = 0
+    vowelList = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
     for i in range(inputlistsize):
+        # cur is current character in list
+        cur = inputlist[i]
+
         # word count
-        if i == ' ':
+        if cur == ' ':
             wordcount = wordcount + 1
+
         # letter count
-        if i != ' ':
+        if (cur >= 'a' and cur <= 'z') or (cur >= 'A' and cur <= 'Z'):
             lettercount = lettercount + 1
+
         # vowel count
-        if i == 'a':
+        if cur in vowelList:
             vowelcount = vowelcount + 1
-        if i == 'e':
-            vowelcount = vowelcount + 1
-        if i == 'i':
-            vowelcount = vowelcount + 1
-        if i == 'o':
-            vowelcount = vowelcount + 1
-        if i == 'u':
-            vowelcount = vowelcount + 1
+
         # sentence count
-        if i == '.':
+        if cur == '.':
             sentencecount = sentencecount + 1
-        if i == '?':
+        elif cur == '?':
             sentencecount = sentencecount + 1
-        if i == '!':
+        elif cur == '!':
             sentencecount = sentencecount + 1
 
-    #countedinput.append(wordcount[0])
-    #countedinput.append(lettercount[1])
-    #countedinput.append(vowelcount[2])
-    #countedinput.append(sentencecount[3])
+    countedinput.append(wordcount)
+    countedinput.append(lettercount)
+    countedinput.append(vowelcount)
+    countedinput.append(sentencecount)
 
-    #return countedinput
+    print(countedinput)
 
-    print (wordcount)
-    print (sentencecount)
-    print (lettercount)
-    print (vowelcount)
-
-    return wordcount
-    return sentencecount
-    return lettercount
-    return vowelcount
+    return countedinput
 
 
 
 def main():
     str = (input('Enter Text:'))
-    text = textsummary(str)
-    #print('Words:' countedinput[0])
-    #print('Characters:' countedinput[1])
-    #print('Vowels:' countedinput[2])
-    #print('Sentences:' countedinput[3])
-
-    #print('Words:' wordcount)
-    #print('Characters:' lettercount)
-    #print('Vowels:' vowelcount)
-    #print('Sentences:' sentencecount)
-
-    print (text)
+    countedlist = textsummary(str)
+    print('Words:', countedlist[0])
+    print('Characters:', countedlist[1])
+    print('Vowels:', countedlist[2])
+    print('Sentences:', countedlist[3])
 
 if __name__ == '__main__':
     main()
